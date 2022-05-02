87cd8fb9302f4d984efa34d80563758e5081ad48
Jordan Jacobson <jordanjacobson@jordans-mbp.myfiosgateway.com>
2022-05-02 13:45:28 -0400

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Routes, Route, Redirect} from "react-router-dom";
+import Homepage from './Components/Homepage'
+import Menu from './Components/Menu'
+import Order from './Components/Order'
+import Reservations from './Components/Reservations';
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div>
+        <Router>
+          <Routes>
+          <Route exact path="/" element={<Homepage/>}/>
+          <Route exact path="/menu" element={<Menu/>}/>
+          <Route exact path="/order" element={<Order/>}/>
+          <Route exact path='/reservations' element = {<Reservations/>}/>
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
