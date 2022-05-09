4cd75f12579df4171a3c04f75c50aeaab39571f6
Jordan Jacobson <jordanjacobson@jordans-mbp.myfiosgateway.com>
2022-05-08 23:43:24 -0400

    setting up modal for order placement

--- src/App.js
+++ src/App.js
@@ -5,23 +5,31 @@ import Homepage from './Components/Homepage'
 import Menu from './Components/Menu'
 import Order from './Components/Order'
 import Reservations from './Components/Reservations';
+import store from './Store/index'
+import { Provider } from 'react-redux';
+import reducer from './Reducers/index';
 
 export default class App extends React.Component {
   constructor(props){
     super(props)
   }
+  componentDidMount(){
+    console.log(typeof(reducer))
+  }
   render(){
     return(
-      <div>
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Homepage/>}/>
-            <Route exact path="/menu" element={<Menu/>}/>
-            <Route exact path="/order" element={<Order/>}/>
-            <Route exact path='/reservations' element = {<Reservations/>}/>
-          </Routes>
-        </Router>
-      </div>
+      <Provider store={store}>
+        <div>
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<Homepage/>}/>
+              <Route exact path="/menu" element={<Menu/>}/>
+              <Route exact path="/order" element={<Order/>}/>
+              <Route exact path='/reservations' element = {<Reservations/>}/>
+            </Routes>
+          </Router>
+        </div>
+      </Provider>
     );
   }
 }
--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react'
+import addItem from '../Actions';
+import reducer from '../Reducers'; 
+import {store} from '../Store'
+import {connect} from 'react-redux'
+import App from '../App';
 
-export default class MenuItem extends React.Component{
+
+class MenuItem extends React.Component{
     constructor(props){
         super(props)
+        this.addToOrder = this.addToOrder.bind(this)
     }
+    addToOrder(itemName, itemPrice){
+        setTimeout(() => {
+            const cartItem = {}
+            cartItem[itemName] = itemPrice
+            this.props.addToCart(cartItem);
+            console.log(this.props.cart);
+            console.log(Object.keys(this.props.cart))
+            console.log(this.props.cart[itemName])
+        }, 10)
+       
+    }
+
     render(){
         return(
             <div>
                <p>
                    {this.props.name}: {' '}
                    {this.props.price}
+                   <button onClick = {() => this.addToOrder(this.props.name, this.props.price)}>Add to order!</button>
                </p>
             </div>
         );
     }
+};
+
+const mapStateToProps = (state) =>{
+    return{
+        cart: state.orders.shoppingCart
+    };
 }
+
+const  mapDispatchToProps = (dispatch) =>{
+    return{
+        addToCart: item => dispatch(addItem(item)),
+    };
+}
+
+export default connect(mapStateToProps,  mapDispatchToProps)(MenuItem)
+
+//export default MenuItem
+
